4b91af3e8ce0fd0d461c4539420e434ff7977370
Brooks Boyd <boydb@midnightdesign.ws>
2013-12-21 15:31:00 -0600

    Functional system!

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Peer = require('./lib/Peer');
 
-var p = new Peer.Peer('dnsseed.bluematt.me');
+var p = new Peer.Peer('dnsseed.bitcoin.dashjr.org');
 
 p.on('connect', function(d) {
   console.log('connect');
@@ -11,17 +11,21 @@ p.on('connect', function(d) {
   m.putInt64(Math.round(new Date().getTime()/1000)); // timestamp
   m.pad(26); // addr_me
   m.pad(26); // addr_you
-  m.putInt64(42); // nonce
+  m.putInt64(0x1234); // nonce
   m.putVarString('Node.js lite peer');
   m.putInt32(10); // start_height
   var raw = m.build('version');
-  console.log(raw);
+  //console.log(DataViewToHex(raw));
+  p.send(DataViewToArray(raw));
 });
 p.on('end', function(d) {
   console.log('end');
 });
 p.on('error', function(d) {
   console.log('error', d);
+});
+p.on('message', function(d) {
+  console.log('message', d.command, DataViewToHex(d.data));
 });
 
 process.on('SIGINT', function() {
@@ -31,3 +35,18 @@ process.on('SIGINT', function() {
 });
 
 p.connect();
+
+function DataViewToArray(dv) {
+  if (dv instanceof DataView === false) return false;
+  return Array.prototype.slice.apply(new Uint8Array(dv.buffer));
+}
+
+function DataViewToHex(dv) {
+  if (dv instanceof DataView === false) return false;
+  return DataViewToArray(dv).map(function(x) { return ('000'+x.toString(16)).slice(-2); }).join('');
+}
+
+function DataViewToBinary(dv) {
+  if (dv instanceof DataView === false) return false;
+  return DataViewToArray(dv).map(function(x) { return String.fromCharCode(x); }).join('');
+}
